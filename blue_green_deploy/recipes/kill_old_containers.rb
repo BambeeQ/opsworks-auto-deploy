aef2677990b5b1a47bd8e004f9079bf21fa1e677
Thamu <thamotharan.g@bambeeq.com>
2015-09-01 16:57:11 +0530

    kill old container updates

--- blue_green_deploy/recipes/kill_old_containers.rb
+++ blue_green_deploy/recipes/kill_old_containers.rb
@@ -5,12 +5,22 @@ script "kill_all_containers" do
   user "root"
   code <<-EOH
 	count=`docker ps -aq | wc -l`
-	if [ $count -eq 0 ]
-	then
-	echo "no continers to remove"
-	else
-        docker ps -aq | xargs -I{} docker exec {} pm2 sendSignal SIGQUIT all
-        docker ps -aq | xargs -I{} docker exec {} pm2 stop all 
+  if [ $count -eq 0 ]
+  then
+     echo "no continers to remove"
+  else
+     container_id=`docker ps -aq`
+     for id in $container_id
+     do
+       status=`docker inspect --format '{{ .State.Running }}' $id`
+       if [ "$status" = "true" ]
+       then
+          docker exec $id pm2 sendSignal SIGQUIT all
+          docker exec $id pm2 stop all
+       else
+          echo "service is not running"
+       fi
+     done
 	fi
   EOH
 end
