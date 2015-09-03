c7dcfd3ef419838a26667616df3e8785595b0db8
Thamu <thamotharan.g@bambeeq.com>
2015-09-03 10:39:37 +0530

    run_tests updates

--- blue_green_deploy/recipes/run_tests.rb
+++ blue_green_deploy/recipes/run_tests.rb
@@ -1,21 +1,28 @@
 require 'chef'
 passing = false
+def checkPassing(data)
+isArray = data.kind_of?(Array)
+if isArray === false
+  data = [data]
+end
+ data.each do |value|
+         if value['passing'] == false
+	  return false
+	 end
+ end
+  return true
+end
+
 node[:stage_ghostscript_url].each do |url|
-rest = Chef::REST.new("#{url}")
-nodes = rest.get_rest("#{url}")
-data = nodes['data']
-passing = checkPassing(data)
+  rest = Chef::REST.new("#{url}")
+  nodes = rest.get_rest("#{url}")
+  data = nodes['data']
+  passing = checkPassing(data)
   if passing == false
     break
   end
 end
-def checkPassing(data)
- data.each do |value|
-         if value['passing'] == false
-         return false
-      end
-  end
-  return true
+
 
 if "#{node[:ignore_failed_tests]}" == 'true'
 Chef::Log.info("Testing")
