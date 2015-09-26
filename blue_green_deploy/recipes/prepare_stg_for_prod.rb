e15568ce7b9748270ee2319ef9734b6c9d13873d
Thamu <thamotharan.g@bambeeq.com>
2015-09-26 19:23:55 +0530

    monitoring instance count updated

--- blue_green_deploy/recipes/prepare_stg_for_prod.rb
+++ blue_green_deploy/recipes/prepare_stg_for_prod.rb
@@ -44,14 +44,23 @@ cron 'process_monitoring' do
   command "/bin/sh -x  /root/monitoring.sh >/dev/null 2>&1"
   action :create
 end
+
+instance_count=1
+node[:opsworks][:layers]["#{node[:submodules][:frontend][:layer]}"][:instances].each do  |value|
+if value[0] == node["opsworks"]["instance"]["hostname"]
 bash "cloud_alarm" do
  user "root"
  group "root"
  code <<-EOH
-aws cloudwatch put-metric-alarm --alarm-name #{node[:prod_env]}_Decider --alarm-description "Alarm when Decider count lessthan #{node[:prod_app_count_threshold]}" --metric-name  OnlineCount --namespace APP_Metrics --statistic Average --period 300 --threshold #{node[:prod_app_count_threshold]} --comparison-operator LessThanThreshold  --dimensions '[{"Name":"InstanceID","Value":"#{node[:opsworks][:instance][:aws_instance_id]}"},{"Name":"Env","Value":"Prod"},{"Name":"App_name","Value":"App"}]' --evaluation-periods 1 --alarm-actions #{node[:arn_sns_details]} --unit Count
-aws cloudwatch put-metric-alarm --alarm-name #{node[:prod_env]}_Disk_Space --alarm-description "Alarm when Disk space exceeds #{node[:prod_disk_space_threshold]} percent" --metric-name  Disk_Space --namespace APP_Metrics --statistic Average --period 300 --threshold #{node[:prod_disk_space_threshold]} --comparison-operator GreaterThanThreshold  --dimensions '[{"Name":"InstanceID","Value":"#{node[:opsworks][:instance][:aws_instance_id]}"},{"Name":"Env","Value":"Prod"},{"Name":"App_name","Value":"Disk_Space"}]' --evaluation-periods 1 --alarm-actions #{node[:arn_sns_details]} --unit Percent
+aws cloudwatch put-metric-alarm --alarm-name #{node[:prod_env]}_App_#{instance_count} --alarm-description "Alarm when App count lessthan #{node[:prod_app_count_threshold]}" --metric-name  OnlineCount --namespace APP_Metrics --statistic Average --period 300 --threshold #{node[:prod_app_count_threshold]} --comparison-operator LessThanThreshold  --dimensions '[{"Name":"InstanceID","Value":"#{node[:opsworks][:instance][:aws_instance_id]}"},{"Name":"Env","Value":"Prod"},{"Name":"App_name","Value":"App"}]' --evaluation-periods 1 --alarm-actions #{node[:arn_sns_details]} --unit Count
+aws cloudwatch put-metric-alarm --alarm-name #{node[:prod_env]}_Disk_Space_#{instance_count} --alarm-description "Alarm when Disk space exceeds #{node[:prod_disk_space_threshold]} percent" --metric-name  Disk_Space --namespace APP_Metrics --statistic Average --period 300 --threshold #{node[:prod_disk_space_threshold]} --comparison-operator GreaterThanThreshold  --dimensions '[{"Name":"InstanceID","Value":"#{node[:opsworks][:instance][:aws_instance_id]}"},{"Name":"Env","Value":"Prod"},{"Name":"App_name","Value":"Disk_Space"}]' --evaluation-periods 1 --alarm-actions #{node[:arn_sns_details]} --unit Percent
 EOH
 end
+end
+instance_count=instance_count+1
+end
+
+
 elsif node[:opsworks][:instance][:layers][0].to_s == "#{node[:submodules][:backend][:layer]}"
 then
 template "/var/www/backend/current/start.sh" do
@@ -124,17 +133,21 @@ cron 'process_monitoring' do
   action :create
 end
 
+instance_count=1
+node[:opsworks][:layers]["#{node[:submodules][:backend][:layer]}"][:instances].each do  |value|
+if value[0] == node["opsworks"]["instance"]["hostname"]
 bash "cloud_alarm" do
  user "root"
  group "root"
  code <<-EOH
-
-aws cloudwatch put-metric-alarm --alarm-name #{node[:prod_env]}_Decider --alarm-description "Alarm when Decider count lessthan #{node[:prod_decider_count_threshold]}" --metric-name  OnlineCount --namespace APP_Metrics --statistic Average --period 300 --threshold #{node[:prod_decider_count_threshold]} --comparison-operator LessThanThreshold  --dimensions '[{"Name":"InstanceID","Value":"#{node[:opsworks][:instance][:aws_instance_id]}"},{"Name":"Env","Value":"Prod"},{"Name":"App_name","Value":"Decider"}]' --evaluation-periods 1 --alarm-actions #{node[:arn_sns_details]} --unit Count
-aws cloudwatch put-metric-alarm --alarm-name #{node[:prod_env]}_Worker --alarm-description "Alarm when Worker count lessthan #{node[:prod_worker_count_threshold]}" --metric-name  OnlineCount --namespace APP_Metrics --statistic Average --period 300 --threshold #{node[:prod_worker_count_threshold]} --comparison-operator LessThanThreshold  --dimensions '[{"Name":"InstanceID","Value":"#{node[:opsworks][:instance][:aws_instance_id]}"},{"Name":"Env","Value":"Prod"},{"Name":"App_name","Value":"Worker"}]' --evaluation-periods 1 --alarm-actions #{node[:arn_sns_details]} --unit Count
-aws cloudwatch put-metric-alarm --alarm-name #{node[:prod_env]}_Disk_Space --alarm-description "Alarm when Disk space exceeds #{node[:prod_disk_space_threshold]} percent" --metric-name  Disk_Space --namespace APP_Metrics --statistic Average --period 300 --threshold #{node[:prod_disk_space_threshold]} --comparison-operator GreaterThanThreshold  --dimensions '[{"Name":"InstanceID","Value":"#{node[:opsworks][:instance][:aws_instance_id]}"},{"Name":"Env","Value":"Prod"},{"Name":"App_name","Value":"Disk_Space"}]' --evaluation-periods 1 --alarm-actions #{node[:arn_sns_details]} --unit Percent
+aws cloudwatch put-metric-alarm --alarm-name #{node[:prod_env]}_Decider_#{instance_count} --alarm-description "Alarm when Decider count lessthan #{node[:prod_decider_count_threshold]}" --metric-name  OnlineCount --namespace APP_Metrics --statistic Average --period 300 --threshold #{node[:prod_decider_count_threshold]} --comparison-operator LessThanThreshold  --dimensions '[{"Name":"InstanceID","Value":"#{node[:opsworks][:instance][:aws_instance_id]}"},{"Name":"Env","Value":"Prod"},{"Name":"App_name","Value":"Decider"}]' --evaluation-periods 1 --alarm-actions #{node[:arn_sns_details]} --unit Count
+aws cloudwatch put-metric-alarm --alarm-name #{node[:prod_env]}_Worker_#{instance_count} --alarm-description "Alarm when Worker count lessthan #{node[:prod_worker_count_threshold]}" --metric-name  OnlineCount --namespace APP_Metrics --statistic Average --period 300 --threshold #{node[:prod_worker_count_threshold]} --comparison-operator LessThanThreshold  --dimensions '[{"Name":"InstanceID","Value":"#{node[:opsworks][:instance][:aws_instance_id]}"},{"Name":"Env","Value":"Prod"},{"Name":"App_name","Value":"Worker"}]' --evaluation-periods 1 --alarm-actions #{node[:arn_sns_details]} --unit Count
+aws cloudwatch put-metric-alarm --alarm-name #{node[:prod_env]}_Disk_Space_#{instance_count} --alarm-description "Alarm when Disk space exceeds #{node[:prod_disk_space_threshold]} percent" --metric-name  Disk_Space --namespace APP_Metrics --statistic Average --period 300 --threshold #{node[:prod_disk_space_threshold]} --comparison-operator GreaterThanThreshold  --dimensions '[{"Name":"InstanceID","Value":"#{node[:opsworks][:instance][:aws_instance_id]}"},{"Name":"Env","Value":"Prod"},{"Name":"App_name","Value":"Disk_Space"}]' --evaluation-periods 1 --alarm-actions #{node[:arn_sns_details]} --unit Percent
 EOH
 end
-
+end
+instance_count=instance_count+1
+end
 
 else
 Chef::Log.warn("Wrong layer selection")
